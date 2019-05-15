da71cd7d56eb58e2302b9745d7926906661dd68e
PaulWebDeveloper <paulworkweb@gmail.com>
2019-05-15 20:39:01 +0300

    feat: add section comments

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ document.getElementById('tab').onclick = () => {
         count = 1;
     }
 };
+
+/**             Slider              */
+const slide = document.querySelectorAll('.slider li');
+const dots = document.querySelectorAll('.dots .radio');
+
+let current = 0;
+let last = 0;
+
+const nextSlide = () => {
+    slide[last].className = '';
+    dots[last].checked = '';
+    current += 1;
+    if (current >= slide.length) current = 0;
+    last = current;
+
+    slide[current].className = 'show';
+    dots[current].checked = 'checked';
+};
+
+const slideInterval = setInterval(nextSlide, 3000);
+
+const toggle = (e) => {
+    current = e - 1;
+    nextSlide();
+};
